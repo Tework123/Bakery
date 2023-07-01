8729bffcc3d8dde3566136ef5a0c7d43539c5c3d
FalconBone <happycatvasiliy9786@gmail.com>
2023-07-01 09:54:03 +0300

    Added frontend folder

---
+++ frontend/bakery-app/src/Components/AppWrapper.jsx
@@ -0,0 +1,11 @@
+import classes from './AppWrapper.module.css';
+
+function AppWrapper() {
+  return (
+    <div className={classes.wrapper}>
+      
+    </div>
+  );
+}
+
+export default AppWrapper;
