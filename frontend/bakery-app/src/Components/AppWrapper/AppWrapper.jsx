2b17b2fd1b2a67518daf56a6688ff3fe822422d8
FalconBone <happycatvasiliy9786@gmail.com>
2023-07-17 18:43:37 +0300

    front_cookie_update

--- frontend/bakery-app/src/Components/AppWrapper/AppWrapper.jsx
+++ frontend/bakery-app/src/Components/AppWrapper/AppWrapper.jsx
@@ -21,24 +21,43 @@ function AppWrapper(props) {
   const [cookies, setCookie] = useCookies(['token']);
 
   //Запрос, на то, зарегался ли человек
-  const { token } = useParams()
+  const { token } = useParams();
   const navigate = useNavigate();
+
 
   console.log(token);
 
   const requestForSuccessfulRegistaration = () => {
-    if (token) {console.log('Отправка данных на сервер')
-      axios.post(`/auth/token`, { token: token }).then((responce) => {
-        console.log('Отправка данных на сервер222222222')
-        props.authorization.authorize(responce.data.data)
-        navigate("/");
-        setCookie('token', token, { path: '/' });
-        console.log('Текущие куки:');
-        console.log(cookies);
-      })
+    if (!props.authorization.isAuthorizated) {
+      if (token) {
+        console.log('Отправка данных на сервер')
+        axios.post(`/auth/token`, { token: token }).then((responce) => {
+          console.log('Обработка данных с сервера')
+          props.authorization.authorize(responce.data.data)
+
+          navigate("/");
+
+          setCookie('token', token, { path: '/' });
+          console.log('Текущие куки:');
+          console.log(cookies);
+        })
+      } else if (cookies) {
+        console.log('Отправка данных на сервер, если есть куки')
+        axios.post(`/auth/token`, { token: cookies.token }).then((responce) => {
+          console.log('Обработка данных с сервера')
+          props.authorization.authorize(responce.data.data)
+
+          navigate("/");
+
+          console.log('Текущие куки:');
+          console.log(cookies);
+        })
+      }
     }
   }
+
   requestForSuccessfulRegistaration()
+  
 
   const test_data = [
 
