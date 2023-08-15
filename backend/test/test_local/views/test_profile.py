3362d55ee9b7a99c436e96a39609e8bbd8f3b1d6
Tework123 <daniliesaec@gmail.com>
2023-08-15 05:46:48 +0300

    fix tests

--- backend/test/test_local/views/test_profile.py
+++ backend/test/test_local/views/test_profile.py
@@ -27,11 +27,15 @@ class TestProfileAuthorized:
         response = client.get('/profile/orders')
 
         assert response.status_code == 200
-        assert response.get_json() == [{'address': None,
-                                        'amount': '1, 1',
+        assert response.get_json() == [{'cards': [{'amount': 1,
+                                                   'image': '/static/%D0%B1%D0%BB%D0%B8%D0%BD%D1%8B%20%D1%81%20%D0%BC%D1%8F%D1%81%D0%BE%D0%BC.jpeg',
+                                                   'name': 'блины с мясом'},
+                                                  {'amount': 1,
+                                                   'image': '/static/%D0%B1%D1%83%D0%BB%D0%BE%D1%87%D0%BA%D0%B0%20%D1%81%20%D0%B2%D0%B8%D1%88%D0%BD%D0%B5%D0%B9.jpeg',
+                                                   'name': 'булочка с вишней'}],
                                         'date': response.get_json()[0]['date'],
-                                        'name': 'блины с мясом, булочка с вишней',
-                                        'order_id': 12,
+                                        'order_id': 13,
+                                        'price': 300,
                                         'status': 'paid',
                                         'text': None}]
 
